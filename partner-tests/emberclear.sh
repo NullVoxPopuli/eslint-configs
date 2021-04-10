a26be1d742f7d9e09cab7af202074b4c4afe7ee6
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-04-10 12:33:33 -0400

    chore(internal): abstract common test behavior from partner tests

--- partner-tests/emberclear.sh
+++ partner-tests/emberclear.sh
@@ -2,33 +2,13 @@
 #
 # -e : exits if any command fails
 # -x : print each command before running it
-#
-set -ex
+# -a : exposes variables to sub shells
 
-REPO="https://github.com/NullVoxPopuli/highlightjs-glimmer.git"
+set -a
+
+REPO="https://github.com/NullVoxPopuli/emberclear.git"
 TEST_DIR="emberclear/client/web"
 
-function quietYarn() {
-  # removes warnings from CLI output
-  yarn $@ 2> >(grep -v warning 1>&2)
-}
+source "$(dirname "${BASH_SOURCE[0]}")/-helpers.sh"
 
-
-yarn link
-
-tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
-echo "Project will be in: $tmp_dir"
-
-cd $tmp_dir
-git clone $REPO
-cd $TEST_DIR
-time quietYarn install --frozen-lockfile
-quietYarn link @nullvoxpopuli/eslint-configs
-
-# Info
-quietYarn list @nullvoxpopuli/eslint-configs
-quietYarn why @nullvoxpopuli/eslint-configs
-ls -la node_modules/@nullvoxpopuli/eslint-configs
-
-# Actually lint
-time yarn eslint . --quiet
+lint
