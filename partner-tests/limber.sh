47ec636c8e9e271885fa084380b6a748cf3b45ea
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-04-11 22:35:38 -0400

    chore(internal): add limber to ci partner tests

---
+++ partner-tests/limber.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# -e : exits if any command fails
+# -x : print each command before running it
+# -a : exposes variables to sub shells
+
+set -a
+
+REPO="https://github.com/NullVoxPopuli/limber.git"
+TEST_DIR="limber"
+
+source "$(dirname "${BASH_SOURCE[0]}")/-helpers.sh"
+
+lint
