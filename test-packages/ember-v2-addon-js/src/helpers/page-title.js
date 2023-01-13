7aece2a154a84cd00daec9fd05289455445932c5
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-01-12 23:03:36 -0500

    Add JS Test V2 Ember Addon

---
+++ test-packages/ember-v2-addon-js/src/helpers/page-title.js
@@ -0,0 +1,41 @@
+import { inject as service } from '@ember/service';
+import Helper from '@ember/component/helper';
+import { guidFor } from '@ember/object/internals';
+
+/**
+  `{{page-title}}` helper used to set the title of the current route context.
+
+  @public
+  @method page-title
+ */
+export default class PageTitle extends Helper {
+  @service('page-title')
+  tokens;
+
+  get tokenId() {
+    return guidFor(this);
+  }
+
+  constructor() {
+    super(...arguments);
+    this.tokens.push({ id: this.tokenId });
+  }
+
+  compute(params, _hash) {
+    let hash = {
+      ..._hash,
+      id: this.tokenId,
+      title: params.join(''),
+    };
+
+    this.tokens.push(hash);
+    this.tokens.scheduleTitleUpdate();
+    return '';
+  }
+
+  willDestroy() {
+    super.willDestroy();
+    this.tokens.remove(this.tokenId);
+    this.tokens.scheduleTitleUpdate();
+  }
+}
