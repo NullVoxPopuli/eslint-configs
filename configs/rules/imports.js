5e62c881a79d3dbadb9bd97e47760f1b694efcb0
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-01-16 14:28:14 -0500

    Disable import lint rule

--- configs/rules/imports.js
+++ configs/rules/imports.js
@@ -18,11 +18,13 @@ const rule = {
     // Anti-foot-gun
     'import/no-cycle': ['error'],
 
+    // Not useful, only stylistic
+    'import/exports-last': 'off',
+
     // Anti-noise
     'import/no-unassigned-import': ['error'],
 
     // Style
-    'import/exports-last': ['error'],
     'import/no-duplicates': ['error'],
     'import/newline-after-import': ['error'],
   },
