4c76c69333be39d1e608ba90990300edad21bd48
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-23 17:05:25 -0500

    wip

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 // @ts-check
-'use strict';
+export { merge, pipe } from './configs/-utils.js';
 
-const { merge, pipe } = require('./configs/-utils');
-
-module.exports = {
-  merge,
-  pipe,
-  configs: {
-    /**
-     * @param {import('./configs/types').Options} [ options ]
-     * @returns {import('eslint').Linter.Config}
-     */
-    ember(options = {}) {
-      return require('./configs/ember').ember(options);
-    },
-    /**
-     * @param {import('./configs/types').Options} [ options ]
-     * @returns {import('eslint').Linter.Config}
-     */
-    crossPlatform(options = {}) {
-      return require('./configs/cross-platform')(options);
-    },
-    /**
-     * @param {import('./configs/types').Options} [ options ]
-     * @returns {import('eslint').Linter.Config}
-     */
-    node(options = {}) {
-      return require('./configs/node').node(options);
-    },
-    /**
-     * @param {import('./configs/types').Options} [ options ]
-     * @returns {import('eslint').Linter.Config}
-     */
-    nodeCJS(options = {}) {
-      return require('./configs/node').nodeCJS(options);
-    },
-    /**
-     * @param {import('./configs/types').Options} [ options ]
-     * @returns {import('eslint').Linter.Config}
-     */
-    nodeESM(options = {}) {
-      return require('./configs/node').nodeESM(options);
-    },
+export const configs = {
+  /**
+   * @param {import('./configs/types').Options} [ options ]
+   * @returns {import('eslint').Linter.Config}
+   */
+  ember(options = {}) {
+    return require('./configs/ember').ember(options);
   },
-};
+  /**
+   * @param {import('./configs/types').Options} [ options ]
+   * @returns {import('eslint').Linter.Config}
+   */
+  crossPlatform(options = {}) {
+    return require('./configs/cross-platform')(options);
+  },
+  /**
+   * @param {import('./configs/types').Options} [ options ]
+   * @returns {import('eslint').Linter.Config}
+   */
+  node(options = {}) {
+    return require('./configs/node').node(options);
+  },
+}; 
