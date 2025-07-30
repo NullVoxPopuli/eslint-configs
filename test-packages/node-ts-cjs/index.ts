e78e6e69df9661e32299aab76bdbea000823578e
Ed Phillips <edmund.phillips@henryscheinone.com>
2025-07-30 14:52:44 +0100

    Test coverage

--- test-packages/node-ts-cjs/index.ts
+++ test-packages/node-ts-cjs/index.ts
@@ -4,4 +4,9 @@ module.exports = {
   foo(two: number) {
     return two;
   },
+  
+  // Test @typescript-eslint/no-unused-vars rule: underscore-prefixed params should be allowed
+  testUnusedVars(_unusedParam: string, used: string) {
+    return used || 'default';
+  },
 };
